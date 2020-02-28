f6b0947d14022dade427ba3fe61292947dece6f1
Brian Zhao <briandzhao@gmail.com>
2020-02-27 17:34:52 -0800

    Set up basic stuff

--- src/MyAI.py
+++ src/MyAI.py
@@ -30,96 +30,53 @@ class MyAI ( Agent ):
 
 		self.hasGold = False
 
-		self.map = []
+		self.map = {}
+		self.frontier = []
+		self.moveStack = []
+		self.undoStack = []
+		self.backTrack = False
 
-		self.numofmoves = 0
+		self.numofMoves = 0
 
 	def getAction( self, stench, breeze, glitter, bump, scream ):
-		self.numofmoves += 1
-		print(self.numofmoves,self.x,self.y)
+		if bump:
+			self.x -= self.dx
+			self.y -= self.dy
 
-		if glitter and not self.hasGold:
+		self.numofMoves += 1
+		print(self.numofMoves,self.x,self.y)
+		self.addToMap(stench,breeze)
+
+		if glitter:
 			return self.grab()
 		if self.x == 0 and self.y == 0:
 			if stench or breeze or self.hasGold:
 				print("CLIMB")
 				return Agent.Action.CLIMB
-
-		if bump:
-			self.x -= self.dx
-			self.y -= self.dy
-
-		return self.forward()
+			else:
+				move = Forward()
+				self.moveStack.append(move)
 
 
-	def isForwardSafe(self):
-		return (self.x + self.dx, self.y + self.dy) in self.safe and self.safe[(self.x + self.dx, self.y + self.dy)]
+		
+		if self.backTrack:
+			return self.undoStack.pop(0).action(self)
+		else:
+			if stench or breeze:
+				self.backTrack = True
+
+		if len(self.moveStack) > 0:
+			move = self.moveStack.pop(0)
+			self.undoStack.append(move)
+			return move.action(self)
+
+		if len(self.frontier) > 0:
+			coord = self.frontier.pop(0)
 
 
 	def addToMap(self, stench, breeze):
-		if len(self.map) <= self.x:
-			self.map.append([])
-		if len(self.map[self.x]) <= self.y:
-			self.map[self.x].append((stench,breeze))
-		if not stench and not breeze and (self.x,self.y) not in self.safe:
-			self.safe[(self.x,self.y)] = not stench and not breeze
-
-
-	def grab(self):
-		self.moved = False
-		self.hasGold = True
-		print("GOLD===========================================================")
-		return Agent.Action.GRAB
-
-
-	def shoot(self):
-		self.moved = False
-		print("SHOOT")
-		return Agent.Action.SHOOT
-
-
-	def forward(self):
-		self.x += self.dx
-		self.y += self.dy
-		self.moved = True
-		print("FORWARD")
-		return Agent.Action.FORWARD
-
-
-	def turnLeft(self):
-		if self.dx == 1:
-			self.dx = 0
-			self.dy = 1
-		elif self.dy == 1:
-			self.dx = -1
-			self.dy = 0
-		elif self.dx == -1:
-			self.dx = 0
-			self.dy = -1
-		else:
-			self.dx = 1
-			self.dy = 0
-		self.moved = False
-		print("TURN_LEFT")
-		return Agent.Action.TURN_LEFT
-
-
-	def turnRight(self):
-		if self.dx == 1:
-			self.dx = 0
-			self.dy = -1
-		elif self.dy == 1:
-			self.dx = 1
-			self.dy = 0
-		elif self.dx == -1:
-			self.dx = 0
-			self.dy = 1
-		else:
-			self.dx = -1
-			self.dy = 0
-		self.moved = False
-		print("TURN_RIGHT")
-		return Agent.Action.TURN_RIGHT
+		if (self.x,self.y) not in self.map:
+			self.map[(self.x,self.y)] = not stench and not breeze
 
 
 	def printStatus(self):
@@ -139,3 +96,72 @@ class MyAI ( Agent ):
 			print()
 			print(self.map)
 		print("---------------")
+
+
+
+class Grab:
+	def action(self, agent):
+		agent.moved = False
+		agent.hasGold = True
+		print("GOLD===========================================================")
+		return Agent.Action.GRAB
+
+class Forward:
+	def __init__(self):
+		print("ASFASFAS")
+
+	def action(self, agent):
+		agent.x += agent.dx
+		agent.y += agent.dy
+		agent.moved = True
+		print("FORWARD")
+		return Agent.Action.FORWARD
+
+	def undo(self, agent):
+		agent.moveStack.append(TurnLeft())
+		agent.moveStack.append(TurnLeft())
+		agent.moveStack.append(Foward())
+
+
+class TurnLeft:
+	def action(self, agent):
+		if agent.dx == 1:
+			agent.dx = 0
+			agent.dy = 1
+		elif agent.dy == 1:
+			agent.dx = -1
+			agent.dy = 0
+		elif agent.dx == -1:
+			agent.dx = 0
+			agent.dy = -1
+		else:
+			agent.dx = 1
+			agent.dy = 0
+		agent.moved = False
+		print("TURN_LEFT")
+		return Agent.Action.TURN_LEFT
+
+	def undo(self, agent):
+		agent.moveStack.append(TurnRight())
+
+
+class TurnRight:
+	def action(self, agent):
+		if agent.dx == 1:
+			agent.dx = 0
+			agent.dy = -1
+		elif agent.dy == 1:
+			agent.dx = 1
+			agent.dy = 0
+		elif agent.dx == -1:
+			agent.dx = 0
+			agent.dy = 1
+		else:
+			agent.dx = -1
+			agent.dy = 0
+		agent.moved = False
+		print("TURN_RIGHT")
+		return Agent.Action.TURN_RIGHT
+
+	def undo(self, agent):
+		agent.moveStack.append(TurnLeft())
